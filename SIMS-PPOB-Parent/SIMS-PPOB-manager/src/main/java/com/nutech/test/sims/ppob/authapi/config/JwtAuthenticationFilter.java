62bf180dbad98733707c7408f72120f7528055f7
Traxman <taufik.rachman.hakim@gmail.com>
2024-12-05 22:59:56 +0700

    Commit to repairing POM.xml

--- SIMS-PPOB-Parent/SIMS-PPOB-manager/src/main/java/com/nutech/test/sims/ppob/authapi/config/JwtAuthenticationFilter.java
+++ SIMS-PPOB-Parent/SIMS-PPOB-manager/src/main/java/com/nutech/test/sims/ppob/authapi/config/JwtAuthenticationFilter.java
@@ -2,6 +2,7 @@ package com.nutech.test.sims.ppob.authapi.config;
 
 import java.io.IOException;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.lang.NonNull;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.Authentication;
@@ -14,30 +15,41 @@ import org.springframework.web.servlet.HandlerExceptionResolver;
 
 import com.nutech.test.sims.ppob.authapi.interfaces.JwtServiceInterface;
 import com.nutech.test.sims.ppob.authapi.interfaces.UserServiceInterface;
-
+import com.nutech.test.sims.ppob.dto.response.error.ResponseHandlingError;
+import com.nutech.test.sims.ppob.response.ResponseGeneral;
 
 import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
 @Component
 @RequiredArgsConstructor
+@Slf4j
 public class JwtAuthenticationFilter extends OncePerRequestFilter {
 	
 	private final HandlerExceptionResolver handlerExceptionResolver;
     private final JwtServiceInterface jwtService;
     private final UserServiceInterface userService;
+    
+    @Autowired
+    private ResponseHandlingError handlingError;
+   
 
     @Override
     protected void doFilterInternal(@NonNull HttpServletRequest request,@NonNull HttpServletResponse response,@NonNull FilterChain filterChain) throws ServletException, IOException {
         final String authHeader = request.getHeader("Authorization");
-
+        
+        log.info("Checking is authHeader :"+authHeader);
+        ResponseGeneral responseGeneral = new ResponseGeneral();
         if (authHeader == null || !authHeader.startsWith("Bearer")) {
-            filterChain.doFilter(request, response);
-            return;
+        	 filterChain.doFilter(request, response);
+        	 responseGeneral = unAuthenticationFaield();
+            //return ;
         }
+        
         try {
             final String jwt = authHeader.substring(7);
             final String userEmail = jwtService.extractUserName(jwt);
@@ -57,6 +69,10 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
                     authToken.setDetails(new WebAuthenticationDetailsSource().buildDetails(request));
                     SecurityContextHolder.getContext().setAuthentication(authToken);
                 }
+            } else  if (userEmail != null && authentication != null) {
+            	// () {
+            		
+            	//}
             }
 
             filterChain.doFilter(request, response);
@@ -65,4 +81,12 @@ public class JwtAuthenticationFilter extends OncePerRequestFilter {
         }
     }
 
+    public ResponseGeneral unAuthenticationFaield() {
+    	ResponseGeneral response = new ResponseGeneral();
+    	response = handlingError.errorUnAuthorized();
+    	
+    	return response;
+    	
+    	
+    }
 }
